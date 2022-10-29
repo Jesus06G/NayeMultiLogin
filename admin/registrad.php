8e0aefe13fe83d6d85ed7dd79e1f9ed71de2ef0e
Jesus <jdgonzalez815@misena.edu.co>
2022-10-28 21:16:48 -0500

    dajisdla

--- admin/registrad.php
+++ admin/registrad.php
@@ -1,16 +1,21 @@
 <?php
 
+
+
 $conexion = mysqli_connect("buojhg5y2w2ultallhhc-mysql.services.clever-cloud.com", "uugunhd73cgyw7jr", "eZ2WtjBrsUn9iloTHA2x", "buojhg5y2w2ultallhhc") or
 die("Problemas con la conexión");
+
+$username= $_POST['username'];
+$email= $_POST['email'];
+$password= $_POST['username'];
+$role= $_POST['role'];
+
   
-      mysqli_query($conexion, "insert into mainlogin(id, username, email, password, role) values 
-        ('$_REQUEST[id]','$_REQUEST[username]','$_REQUEST[email]',$_REQUEST[password],'$_REQUEST[role]')")
+      mysqli_query($conexion, "insert into mainlogin( username, email, password, role) values 
+        ($username,$email,$password,$role)")
         or die("Problemas en el select" . mysqli_error($conexion));
-    
-      mysqli_close($conexion);
-    
-      echo "Ingreso Correctamente.";
-      
-header("location: admin_portada.php");
+
+        header("location: admin_portada.php");
+      mysqli_close($conexion);     
       ?>
   
